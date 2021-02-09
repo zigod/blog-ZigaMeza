7c7b97cad5a913bdb092e9cfc1d49e8457bdb2bb
zigod <ziga.meza109@gmail.com>
2021-02-09 18:56:04 +0100

    Hmmm men se zdi da use dela tk ko po videu.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :news
+  resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'homes#index'
 end
