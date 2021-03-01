58246f14608bf69fea5478b4614c3496b0ba291c
zigod <ziga.meza109@gmail.com>
2021-03-01 22:22:05 +0100

    Establish user posts connection

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_01_205525) do
+ActiveRecord::Schema.define(version: 2021_03_01_212019) do
 
   create_table "homes", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -24,12 +24,8 @@ ActiveRecord::Schema.define(version: 2021_03_01_205525) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+# Could not dump table "posts" because of following StandardError
+#   Unknown type 'reference' for column 'user_id'
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
